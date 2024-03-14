931ca62802c97f10b176d3e4a3a1c6285e628ed1
fezzay <thomasjccooper@gmail.com>
2024-03-14 20:03:20 +0000

    added force graph with json loading data

--- Fuze/src/app/Dashboard/node.dashboard.graph/node.dashboard.graph.component.ts
+++ Fuze/src/app/Dashboard/node.dashboard.graph/node.dashboard.graph.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as fg from 'force-graph';
 @Component({
   selector: 'node-dashboard-graph',
   templateUrl: './node.dashboard.graph.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class NodeDashboardGraphComponent {
 
+  constructor() {}
+
+  ngOnInit(): void {
+    fetch('assets/fakeData.json').then(res => res.json()).then(data => {
+      const graph = fg.default()
+    (document.getElementById('graph')!)
+      .graphData(data)
+    });
+  }
 }
